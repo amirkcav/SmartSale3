817aa4a051673c10009430f78aad25f2db52c26f
amirkcav <amir.karasik@cavsystems.com>
2019-06-11 11:49:13 +0300

    confirmation before leaving page

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ng build --environment=prod --base-href /smartsale/dist/  --target=production
+ng build --environment=prod --base-href /smartsale/dist/ --aot # --target=production
 scp -r dist cache.cav.local:/cav/tomcat7/webapps/smartsale/
 scp signin.css index.jsp cache.cav.local:/cav/tomcat7/webapps/smartsale/
